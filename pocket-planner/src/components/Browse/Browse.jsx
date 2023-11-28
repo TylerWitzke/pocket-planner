6280bd3aa3505daa1b03475ba5381035f190924e
Harry <77893070+harrisonMondragon@users.noreply.github.com>
2023-11-28 01:26:04 -0700

    Fix up items
    Items should be good (enough) to go for now. To see an example, look at
    the browse page

--- pocket-planner/src/components/Browse/Browse.jsx
+++ pocket-planner/src/components/Browse/Browse.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import './Browse.css'
 import Item from '../Item/Item'
-import pocketPlannerImage from '../assets/pocket-planner.png'
 import calawayAttraction from '../assets/calaway_attraction.jpg'
+import blueRingAttraction from '../assets/blue_ring_attraction.jpg'
 
 const Browse = () => {
   return (
     <div className='outer-container'>
       <Item
-        onClick={()=>{console.log("fook item 1")}}
-        title="My sick example item"
-        info="This example item is so dope"
-        image={calawayAttraction}/>
+        onClick={()=>{console.log("Calaway Park clicked")}}
+        title="Calaway Park"
+        info1="Amusement Park - 32.2 km"
+        info2="Open 24 Hours"
+        info3="5/5 Stars"
+        picture={calawayAttraction}/>
 
       <Item
-        onClick={()=>{console.log("fook item 2")}}
-        title="My less sick other example item"
-        info="This example item is less dope than the first"
-        image={pocketPlannerImage}/>
+        onClick={()=>{console.log("Big Blue Ring clicked")}}
+        title="Big Blue Ring"
+        info1="Art Piece - 16 km"
+        info2="Open 10am - 6pm"
+        info3="4.5/5 Stars"
+        picture={blueRingAttraction}/>
     </div>
   )
 }
--- pocket-planner/src/components/Item/Item.jsx
+++ pocket-planner/src/components/Item/Item.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import './Item.css'; // Import the CSS file
 
-const Item = ({onClick, title, info, picture}) => {
+const Item = ({onClick, title, info1, info2, info3, picture}) => {
   return (
     <div className='item-container-style' onClick={onClick}>
       <div className='item-text-content-style'>
-        <h2>{title}</h2>
-        <p>{info}</p>
+        <h2 className='item-title-style'>{title}</h2>
+        <p className='item-info-style'>{info1}</p>
+        <p className='item-info-style'>{info2}</p>
+        <p className='item-info-style'>{info3}</p>
       </div>
       <img src={picture} alt={title} className='item-picture-style' />
       </div>
