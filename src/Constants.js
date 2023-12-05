7f160302b721bfe508824ffab78e1d0875ce8513
Harry <77893070+harrisonMondragon@users.noreply.github.com>
2023-12-05 00:11:20 -0700

    Improve data
    Amenities are good now

--- src/Constants.js
+++ src/Constants.js
@@ -11,6 +11,12 @@ import dirty_dancing_event from './components/assets/dirty_dancing_event.jpg'
 import calgary_hitmen_event from './components/assets/calgary_hitmen_event.png'
 import morgan_wallen_event from './components/assets/morgan_wallen_event.jpg'
 
+import barbarella_amenity from './components/assets/barbarella_amenity.jpg'
+import anytime_fitness_amenity from './components/assets/anytime_fitness_amenity.jpg'
+import market_mall_amenity from './components/assets/market_mall_amenity.jpg'
+import foothills_medical_amenity from './components/assets/foothills_medical_amenity.jpg'
+import seven_eleven_amenity from './components/assets/seven_eleven_amenity.jpg'
+
 // Start attractions -----------------------------------------------------
 const bigBlueRing = {
     onClick: () => { console.log("bigBlueRing"); },
@@ -120,114 +126,110 @@ const bigBlueRingInfo = {
 // End attractions -----------------------------------------------------
 
 // Start amenities -----------------------------------------------------
-const bigBlueRing2 = {
-  onClick: () => { console.log("bigBlueRing"); },
-  title: "Big Blue Ring Amen",
-  info1: "Art Piece - 16km",
-  info2: "Open 24 hours",
+const barbarella = {
+  title: "Barbarella Bar",
+  info1: "Resturaunt - 2km",
+  info2: "Open 11am - 10pm",
   info3: "Rating: 5/5",
-  picture: big_blue_ring_attraction,
+  picture: barbarella_amenity,
   id: 0,
   date: null
 };
 
-const calawayPark2 = {
-  onClick: () => { console.log("calawayPark"); },
-  title: "Calaway Park Amen",
-  info1: "Amusement Park - 54.5 km",
-  info2: "Open 10am - 9pm",
+const anytimeFitness = {
+  title: "AnyTime Fitness",
+  info1: "Gym - 5km",
+  info2: "Open 24 hours",
   info3: "Rating: 4/5",
-  picture: calaway_park_attraction,
+  picture: anytime_fitness_amenity,
   id: 1,
   date: null
 };
 
-const calgaryTower2 = {
-  onClick: () => { console.log("calgaryTower"); },
-  title: "Calgary Tower Amen",
-  info1: "Iconic building - 16km",
-  info2: "Open  10am - 1am",
-  info3: "Rating: 4/5",
-  picture: calgary_tower_attraction,
+const marketMall = {
+  title: "Market Mall",
+  info1: "Shopping Mall - 7.2km",
+  info2: "Open  10am - 9pm",
+  info3: "Rating: 3.5/5",
+  picture: market_mall_amenity,
   id: 2,
   date: null
 };
 
-const heritagePark2 = {
-  onClick: () => { console.log("heritagePark"); },
-  title: "Heritage Park Amen",
-  info1: "Amusement park - 3.2km",
-  info2: "Open 8am to 6pm",
+const foothillsMedical = {
+  title: "Foothills Medical Center",
+  info1: "Hospital - 11km",
+  info2: "Open 24 hours",
   info3: "Rating: 2/5",
-  picture: heritage_park_attraction,
+  picture: foothills_medical_amenity,
   id: 3,
   date: null
 };
 
-const olympicPlaza2 = {
-  onClick: () => { console.log("olympicPlaza"); },
-  title: "Olympic Plaza Amen",
-  info1: "Skating rink - 10km",
+const sevenEleven= {
+  title: "7-Eleven",
+  info1: "Convenience Store - 13.4km",
   info2: "Open 24 hours",
   info3: "Rating: 2/5",
-  picture: oplympic_plaza_attraction,
+  picture: seven_eleven_amenity,
   id: 4,
   date: null
 };
 
-export const amenitie_items = [bigBlueRing2,calawayPark2,calgaryTower2,heritagePark2,olympicPlaza2]
+export const amenitie_items = [barbarella,anytimeFitness,marketMall,foothillsMedical,sevenEleven]
 
-const bigBlueRingInfo2 = {
-  title: 'Big Blue Ring Amen',
-  picture: big_blue_ring_attraction,
-  description: 'Gorgeous massive blue ring. It only took $471,000 to construct this masterpiece. Referred to as Calgary’s pride and joy by locals.',
-  locationName: 'Deerfoot Trail',
-  locationDistance: '16 km away',
+const barbarellaInfo = {
+  title: "Barbarella Bar",
+  description: 'Upscale casual restaurant in downtown Calgary serving you vibrant coastal cuisine and an escape to the Italian South, circa 1968',
+  locationName: '240 8 Ave SW, Calgary, AB',
+  locationDistance: '2 km away',
   contact: '(403)-111-5222',
+  hours: "Open 11am - 10pm",
+  picture: barbarella_amenity,
+  id: 0,
+};
+
+const anytimeFitnessInfo = {
+  title: "AnyTime Fitness",
+  picture: anytime_fitness_amenity,
+  description: 'With a focus on convenience and personalized fitness plans, Anytime Fitness aims to create a welcoming and supportive environment for individuals pursuing their health and wellness goals',
+  locationName: '722 85 St SW, Calgary, AB',
+  locationDistance: '5 km away',
+  contact: '(403)-999-0232',
   hours: 'Open 24 hours',
 };
 
-const calawayParkInfo2 = {
-  title: 'Calaway Park Amen',
-  picture: calaway_park_attraction,
-  description: 'Popular amusement park located just west of Calgary, Alberta, Canada. It is the largest outdoor family amusement park in Western Canada.',
-  locationName: '245033 Range Rd 33',
-  locationDistance: '54.5 km away',
-  contact: '(403)-999-0232',
-  hours: 'Open 10am - 9pm',
-};
-
-const calgaryTowerInfo2 = {
-  title: 'Calgary Tower Amen',
-  picture: calgary_tower_attraction,
-  description: 'The Calgary Tower is an iconic landmark in downtown Calgary, Alberta, Canada. Standing at 191 meters, it offers panoramic views of the city and the surrounding Rocky Mountains.',
-  locationName: 'Deerfoot Trail',
-  locationDistance: '16 km away',
+const marketMallInfo = {
+  title: "Market Mall",
+  picture: market_mall_amenity,
+  description: 'Boasting a diverse range of retail stores, dining options, and entertainment facilities, Market Mall offers a comprehensive shopping experience for visitors in the city.',
+  locationName: '3625 Shaganappi Trail NW, Calgary, AB',
+  locationDistance: '7.2 km away',
   contact: '(403)-111-2222',
-  hours: 'Open  10am - 1am',
+  hours: 'Open  10am - 9pm',
 };
 
-const heritageParkInfo2 = {
-  title: 'Heritage Park Amen',
-  picture: heritage_park_attraction,
-  description: 'Heritage Park in Calgary, Alberta, is a living history museum that beautifully captures the essence of Western Canada’s past.',
+const foothillsMedicalInfo = {
+  title: "Foothills Medical Center",
+  picture: foothills_medical_amenity,
+  description: 'As one of the largest hospitals in the region, it serves as a hub for medical services, research, and education, providing comprehensive healthcare to the community and playing a crucial role in the Alberta health system.',
   locationName: '1900 Heritage Dr SW',
-  locationDistance: '3.2 km away',
+  locationDistance: '11 km away',
   contact: '(587)-411-1122',
-  hours: 'Open 8am to 6pm',
+  hours: 'Open 24 hours',
 };
 
-const olympicPlazaInfo2 = {
-  title: 'Olympic Plaza Amen',
-  picture: oplympic_plaza_attraction,
-  description: 'Olympic Plaza in Calgary, Alberta, is a vibrant public space with historical significance as the site of the medal presentations during the 1988 Winter Olympics.',
-  locationName: '228 8 Ave SE',
-  locationDistance: '10 km away',
+const sevenElevenInfo = {
+  title: "7-Eleven",
+  picture: seven_eleven_amenity,
+  description: 'Offering a wide array of snacks, beverages, and everyday essentials, 7-Eleven is recognized for its convenience and accessibility, with numerous locations worldwide providing customers with 24/7 service.',
+  locationName: '6408 Old Banff Coach Rd SW, Calgary, AB',
+  locationDistance: '13.4 km away',
   contact: '(587)-111-2922',
   hours: 'Open 24 hours',
 };
 
-export const amenitie_items_info = [bigBlueRingInfo2, calawayParkInfo2, calgaryTowerInfo2, heritageParkInfo2, olympicPlazaInfo2]
+export const amenitie_items_info = [barbarellaInfo,anytimeFitnessInfo,marketMallInfo,foothillsMedicalInfo,sevenElevenInfo]
 
 // End amenities -----------------------------------------------------
 
